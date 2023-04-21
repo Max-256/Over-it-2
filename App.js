3c9c9ef8a87b31a97669e86908fe1a92a58ff2f8
Opio Maxwell <opiomaxwell11122@gmail.com>
2023-04-21 19:07:35 +0300

    build the image input component

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import * as ImagePicker from "expo-image-picker";
 
 import Screen from "./app/components/Screen";
 import { Button, Image } from "react-native";
+import ImageInput from "./app/components/ImageInput";
 
 export default function App() {
   const [imageUri, setImageUri] = useState();
@@ -18,13 +19,14 @@ export default function App() {
 
   const selectImage = async () => {
     const result = await ImagePicker.launchImageLibraryAsync();
-    if (!result.canceled) setImageUri(result.uri);
+    if (!result.canceled) setImageUri(result.assets[0].uri);
   };
 
   return (
     <Screen>
       <Button title="select image" onPress={selectImage} />
       <Image source={{ uri: imageUri }} style={{ width: 200, height: 200 }} />
+      <ImageInput imageUri={imageUri} />
     </Screen>
   );
 }
