90f268b9030034ee2a8b971ac84cd8026f3be662
Opio Maxwell <opiomaxwell11122@gmail.com>
2023-04-21 15:18:58 +0300

    request access  permission

--- App.js
+++ App.js
@@ -1,6 +1,17 @@
-import React from "react";
-import ListingEditScreen from "./app/screens/ListingEditScreen";
+import React, { useEffect } from "react";
+import * as ImagePicker from "expo-image-picker";
+
+import Screen from "./app/components/Screen";
 
 export default function App() {
-  return <ListingEditScreen />;
+  const requestPermission = async () => {
+    const { granted } = await ImagePicker.requestCameraPermissionsAsync();
+    if (!granted) alert("You need to enable permision to access the library");
+  };
+
+  useEffect(() => {
+    requestPermission();
+  }, []);
+
+  return <Screen></Screen>;
 }
