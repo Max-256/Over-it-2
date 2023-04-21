527e659473c158473d05be344756b058ca5f42de
Opio Maxwell <opiomaxwell11122@gmail.com>
2023-04-21 15:36:22 +0300

    add plugins to app.json

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import * as ImagePicker from "expo-image-picker";
 
 import Screen from "./app/components/Screen";
+import { Button, Image } from "react-native";
 
 export default function App() {
+  const [imageUri, setImageUri] = useState();
+
   const requestPermission = async () => {
     const { granted } = await ImagePicker.requestCameraPermissionsAsync();
     if (!granted) alert("You need to enable permision to access the library");
@@ -13,5 +16,15 @@ export default function App() {
     requestPermission();
   }, []);
 
-  return <Screen></Screen>;
+  const selectImage = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync();
+    if (!result.canceled) setImageUri(result.uri);
+  };
+
+  return (
+    <Screen>
+      <Button title="select image" onPress={selectImage} />
+      <Image source={{ uri: imageUri }} style={{ width: 200, height: 200 }} />
+    </Screen>
+  );
 }
