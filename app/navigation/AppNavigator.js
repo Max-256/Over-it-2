b342c7e6d113a5e6f4bfdc62f5212791bffc4916
Opio Maxwell <opiomaxwell11122@gmail.com>
2023-04-25 22:57:10 +0300

    build new listing navigation button

--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -1,8 +1,11 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
 import ListingEditScreen from "../screens/ListingEditScreen";
 import FeedNavigator from "./FeedNavigator";
 import AccountNavigator from "./AccountNavigator";
+import NewListingButton from "./NewListingButton";
 
 const Tab = createBottomTabNavigator();
 
@@ -12,9 +15,38 @@ const AppNavigator = () => (
       headerShown: false,
     }}
   >
-    <Tab.Screen name="Feed" component={FeedNavigator} />
-    <Tab.Screen name="ListingEdit" component={ListingEditScreen} />
-    <Tab.Screen name="Account" component={AccountNavigator} />
+    <Tab.Screen
+      name="Feed"
+      component={FeedNavigator}
+      options={{
+        tabBarIcon: ({ color, size }) => (
+          <MaterialCommunityIcons name="home" color={color} size={size} />
+        ),
+      }}
+    />
+    <Tab.Screen
+      name="ListingEdit"
+      component={ListingEditScreen}
+      options={{
+        tabBarButton: () => <NewListingButton />,
+        tabBarIcon: ({ color, size }) => (
+          <MaterialCommunityIcons
+            name="plus-circle"
+            color={color}
+            size={size}
+          />
+        ),
+      }}
+    />
+    <Tab.Screen
+      name="Account"
+      component={AccountNavigator}
+      options={{
+        tabBarIcon: ({ color, size }) => (
+          <MaterialCommunityIcons name="account" color={color} size={size} />
+        ),
+      }}
+    />
   </Tab.Navigator>
 );
 
---
+++ app/navigation/NewListingButton.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import colors from "../config/colors";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { TouchableOpacity } from "react-native-gesture-handler";
+
+function NewListingButton({ onPress }) {
+  return (
+    <TouchableOpacity style={styles.container} onPress={onPress}>
+      <MaterialCommunityIcons
+        name="plus-circle"
+        color={colors.white}
+        size={40}
+      />
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    backgroundColor: colors.primary,
+    borderRadius: 40,
+    bottom: 20,
+    borderColor: colors.white,
+    borderWidth: 7,
+    height: 70,
+    justifyContent: "center",
+    width: 70,
+  },
+});
+
+export default NewListingButton;
