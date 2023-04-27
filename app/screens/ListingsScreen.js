fd076ea72a71d69a21a80f2e301d9b2ffbaa7bf9
Opio Maxwell <opiomaxwell11122@gmail.com>
2023-04-28 01:10:55 +0300

    handle errors

--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -1,17 +1,25 @@
 import React, { useEffect, useState } from "react";
 import { FlatList, StyleSheet } from "react-native";
+import Button from "../components/Button";
 import { getListings } from "../api/listings";
 
 import Card from "../components/Card";
 import colors from "../config/colors";
 import Screen from "../components/Screen";
+import AppText from "../components/Text";
 
 function ListingsScreen({ navigation }) {
   const [listings, setListings] = useState([]);
+  const [error, setError] = useState(false);
 
   const LoadListings = async () => {
-    const { data } = await getListings();
-    setListings(data);
+    const response = await getListings();
+    if (!response.ok) {
+      return setError(true);
+    }
+
+    setError(false);
+    setListings(response.data);
   };
 
   useEffect(() => {
@@ -20,6 +28,12 @@ function ListingsScreen({ navigation }) {
 
   return (
     <Screen style={styles.screen}>
+      {error && (
+        <>
+          <AppText>Could not retrieve the listings.</AppText>
+          <Button title="retry" onPress={LoadListings} />
+        </>
+      )}
       <FlatList
         data={listings}
         keyExtractor={(listing) => listing.id.toString()}
@@ -38,7 +52,7 @@ function ListingsScreen({ navigation }) {
 
 const styles = StyleSheet.create({
   screen: {
-    padding: 3,
+    padding: 5,
     backgroundColor: colors.light,
   },
 });
