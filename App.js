33e9a9a79a90f0c828db443eb3b04398621a50d6
Opio Maxwell <opiomaxwell11122@gmail.com>
2023-04-30 20:03:39 +0300

    add async storage

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import navigationTheme from "./app/navigation/navigationTheme";
 import AppNavigator from "./app/navigation/AppNavigator";
 import NetInfo, { useNetInfo } from "@react-native-community/netinfo";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function App() {
   const netInfo = useNetInfo();
