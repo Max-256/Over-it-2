58c50b01ea19c8e892f9278dff12e16723cd466c
Opio Maxwell <opiomaxwell11122@gmail.com>
2023-04-30 19:44:47 +0300

    detect network status

--- App.js
+++ App.js
@@ -2,11 +2,17 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import navigationTheme from "./app/navigation/navigationTheme";
 import AppNavigator from "./app/navigation/AppNavigator";
+import NetInfo, { useNetInfo } from "@react-native-community/netinfo";
 
 export default function App() {
+  const netInfo = useNetInfo();
+  console.log(netInfo.isInternetReachable);
+
   return (
-    <NavigationContainer theme={navigationTheme}>
-      <AppNavigator />
-    </NavigationContainer>
+    // <NavigationContainer theme={navigationTheme}>
+    //   <AppNavigator />
+    // </NavigationContainer>
+
+    null
   );
 }
